c55f9f3a1413c719c2b151b86e474f90817cc089
Arina <arinaligit@gmail.com>
2023-07-21 13:22:37 +0300

    add func fetchAlbum

--- iTunesTestTask/SceneDelegate.swift
+++ iTunesTestTask/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let myScene = (scene as? UIWindowScene) else { return }
         window = UIWindow (windowScene: myScene)
-        window?.rootViewController = UINavigationController(rootViewController: AuthViewController())
+        window?.rootViewController = AuthViewController()
         window?.makeKeyAndVisible()
     }
 
